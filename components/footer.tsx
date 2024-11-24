157884ba020e2c2d6931a2218e3f93e76e1e7f41
Stephen Melnicki <hello@stephenmelnicki.com>
2024-11-23 19:09:33 -0800

    header and footer styling update

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,30 +1,29 @@
+import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 
 export function Footer() {
   return (
-    <footer className="mt-6 flex flex-col-reverse flex-wrap items-center justify-center gap-4 text-sm text-gray-600 sm:mt-8 sm:flex-row sm:justify-between">
-      <span>&copy; 2024 Stephen Melnicki</span>
+    <footer className="mt-6 flex flex-col-reverse flex-wrap items-center justify-center gap-4 text-sm sm:mt-8 sm:flex-row sm:justify-between">
+      <span className="py-2">&copy; 2024 Stephen Melnicki</span>
       <nav>
         <ul className="flex flex-row items-center justify-center gap-4">
           <li>
-            <Link
-              className="p-2 hover:text-blue-600 hover:underline"
-              href="https://github.com/stephenmelnicki/nextpaste"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              Source
-            </Link>
+            <Button variant="link">
+              <Link href="https://github.com/stephenmelnicki/nextpaste" rel="noopener noreferrer" target="_blank">
+                Source
+              </Link>
+            </Button>
           </li>
           <li>
-            <Link
-              className="p-2 hover:text-blue-600 hover:underline"
-              href="https://raw.githubusercontent.com/stephenmelnicki/nextpaste/main/LICENSE"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              License
-            </Link>
+            <Button variant="link">
+              <Link
+                href="https://raw.githubusercontent.com/stephenmelnicki/nextpaste/main/LICENSE"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                License
+              </Link>
+            </Button>
           </li>
         </ul>
       </nav>
--- components/header.tsx
+++ components/header.tsx
@@ -8,7 +8,7 @@ export function Header() {
         <h1 className="text-3xl font-bold">
           <Link href="/">Next Paste</Link>
         </h1>
-        <p className="mt-1 text-gray-600">A simple paste service built with Next.js and Vercel</p>
+        <p className="mt-1">A simple paste service built with Next.js and Vercel</p>
       </hgroup>
       <nav>
         <ThemeToggle />
